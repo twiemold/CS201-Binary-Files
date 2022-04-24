8bc07fe8d139d7370da82471ff1b1b6c8ab9020f
twiemold <twiemold@uvm.edu>
2022-04-24 16:24:31 -0400

    added utility functions from starter code

--- fileops.twiemold.c
+++ fileops.twiemold.c
@@ -7,8 +7,93 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXWORDLEN 64
+
+//--------------------------------------------------------
+// return 1 if any chararacters are non-alphabetic;
+// otherwise return 0
+
+int checkWord(char *word) {
+    int i, len;
+
+    len = strlen(word);
+    for (i=0; i<len; ++i) {
+        if ( ! isalpha(word[i]) )
+            return 1;
+    }
+
+    return 0;
+}
+
+//--------------------------------------------------------
+// convert all characters to their lower-case equivalents;
+// leaves characters that are already lower case unchanged
+// returns zero always
+
+int convertToLower(char *word, char *convertedWord) {
+    int i, len;
+
+    strcpy(convertedWord, word);
+
+    len = strlen(word);
+    for (i=0; i<len; ++i)
+        convertedWord[i] = tolower(word[i]);
+
+    return 0;
+}
+
+//--------------------------------------------------------
+
+int test_tolower(char *word) {
+    int len, i, val;
+    char newword[MAXWORDLEN];
+
+    len = strlen(word);
+    strcpy(newword, word);
+    for (i=0; i<len; ++i) {
+        newword[i] = tolower(word[i]);
+    }
+    printf("newword is |%s|\n", newword);
+    return 0;
+}
+
+//--------------------------------------------------------
+
+int testUtils() {
+    char buffer[MAXWORDLEN];
+    char convertedBuffer[MAXWORDLEN];
+    int rc;
+
+    test_tolower("Nargles");
+
+//strcpy(buffer, "nargles");
+//test_tolower(buffer);
+
+    strcpy(buffer, "John's");
+    rc = checkWord(buffer);
+    printf("%s: %d\n", buffer, rc);
+
+    strcpy(buffer, "maryRose");
+    rc = checkWord(buffer);
+    printf("%s: %d\n", buffer, rc);
+    if (rc) {
+        convertToLower(buffer, convertedBuffer);
+        printf("converted: |%s|\n", convertedBuffer);
+    }
+
+    strcpy(buffer, "mary Rose");
+    rc = checkWord(buffer);
+    printf("%s: %d\n", buffer, rc);
+
+    return 0;
+}
+
+//-------------------------------------
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  testUtils();
 }
--- fileops.twiemold.h
+++ fileops.twiemold.h
@@ -10,4 +10,12 @@
 #ifndef CS201BINARYFILES_FILEOPS_TWIEMOLD_H
 #define CS201BINARYFILES_FILEOPS_TWIEMOLD_H
 
+// Utility Functions
+int checkWord(char *word);
+int convertToLower(char *word, char *convertedWord);
+int test_tolower(char *word);
+
+// Testing functions
+int testUtils();
+
 #endif //CS201BINARYFILES_FILEOPS_TWIEMOLD_H
